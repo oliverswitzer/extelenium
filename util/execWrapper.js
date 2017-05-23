c18d32ffcb61a884b1306de5827c8e5c11c39ca5
Oliver Switzer <pair+oswitzer@pivotal.io>
2017-05-23 14:49:50 -0400

    Increase maxBuffer of exec command so that stdout can always be read for larger base64 .crx files

--- util/execWrapper.js
+++ util/execWrapper.js
@@ -1,5 +1,11 @@
 const exec = require('child_process').exec
 
 module.exports = (command, callback) => {
-  exec(command, callback)
+  exec(command, { maxBuffer: MAX_CRX_SIZE * SIZE_ADDED_BY_ENCODING }, callback)
 }
+
+const KB = 1024
+const GiB = 1e6*KB
+
+const MAX_CRX_SIZE = 2*GiB
+const SIZE_ADDED_BY_ENCODING = 4/3
